e7f7ee4e6fc4d98b2e2c0e32f543db5d854dd0fe
Lucian Aionicesei <lucian.aionicesei@gmail.com>
2022-02-09 21:10:15 +0100

    last commit

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+
+let i = 1;
+const countingArray = [0];
+
+loop();
+
+function loop() {
+
+    // console.log(countingArray.length);
+    // console.log(i);
+    console.log(countingArray);
+    if (countingArray.length < 9) {
+        countingArray.unshift(i);
+    } else {
+        countingArray.unshift(i);
+        countingArray.pop(-1);
+    }
+
+    if ( i < 15) {
+        setTimeout(function () {
+            i++
+            loop();
+        }, 1000);
+    }
+}
